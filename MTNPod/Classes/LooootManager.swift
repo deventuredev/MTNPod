ba53e951e391cc8ac0c833b7e4bb181d53043099
Ionascut Mihai <ionascut.mihai@deventure.co>
2021-04-15 09:45:46 +0300

    release mtn 1.0.1

--- MTNPod/Classes/LooootManager.swift
+++ MTNPod/Classes/LooootManager.swift
@@ -5,12 +5,9 @@ import GoogleMaps
 public class LooootManager : BaseLooootManager, MapViewDelegate {
     var mapView: MTNMapView?
     
-    public static var shared:BaseLooootManager
-    {
-        get
-        {
-            if(BaseLooootManager.sharedInstance.protoHttpManagerDelegate == nil)
-            {
+    public static var shared:BaseLooootManager {
+        get {
+            if(BaseLooootManager.sharedInstance.protoHttpManagerDelegate == nil) {
                 BaseLooootManager.sharedInstance.protoHttpManagerDelegate = ProtoClientManager.shared
             }
             return BaseLooootManager.sharedInstance
@@ -19,7 +16,6 @@ public class LooootManager : BaseLooootManager, MapViewDelegate {
     
     public func GetMapView() -> BaseMapView {
         GMSServices.provideAPIKey(LooootManager.googleMapsApiKey)
-        
         return mapView!
     }
     
